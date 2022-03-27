2fe1180e5f9a46d11bc4796694cd4531cbd0e644
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2022-03-27 15:16:31 +0500

    add reducer rang tanlash rgb

--- vitrina_react/src/App.jsx
+++ vitrina_react/src/App.jsx
@@ -1,10 +1,36 @@
- 
-import './App.css';
+ import { useReducer } from 'react';
+ import './App.css';
+ import reducer from "./reducer"
 
-function App() {
+ function App() {
+ const [{r,g,b},dispatch] = useReducer(reducer,{r:0,g:0,b:255});
 
-  return (
- <></>
-  );
-}
-export default App;
+ return (
+ <div className='App' style={{background:`rgb(${r+200},${g }, ${b })`}}>
+   <h1 style={{color:`rgb(${r},${g}, ${b})`}}>useImperativeHandle Hook</h1>
+   <div>
+     <span>
+       R
+     </span>
+     <button onClick={() => dispatch({type:"Increment_R"})}>
+       +</button>
+     <button onClick={() => dispatch({type:"Decrement_R"})}>
+       -
+     </button>
+   </div>
+   <div>
+     <span>
+       G
+     </span>
+     <button onClick={() => dispatch({type:"Increment_G"})}>+</button>
+     <button  onClick={() => dispatch({type:"Decrement_G"})} >-</button>
+   </div>
+   <div>
+     <span>B</span>
+     <button onClick={() => dispatch({type:"Increment_B"})}>+</button>
+     <button  onClick={() => dispatch({type:"Decrement_B"})}>-</button>
+   </div>
+ </div>
+ );
+ }
+ export default App;
--- vitrina_react/src/Reducer.jsx
+++ vitrina_react/src/Reducer.jsx
@@ -1,1 +1,41 @@
-const reducer = (state,action)
+const reducer = (state,action) =>{
+    const limitRGB =(num) => (num<0 ? 0 : num>256 ? 256 : num);
+    const step = 50;
+
+    switch(action.type){
+        case "Increment_R":
+            return{
+                ...state,
+                r:limitRGB(state.r+step)
+            };
+        case "Decrement_R": 
+            return{
+                ...state,
+                r:limitRGB(state.r-step),
+            };
+        case "Increment_G":
+            return{
+                ...state,
+                g:limitRGB(state.g+step)
+            };
+        case "Decrement_G": 
+            return{
+                ...state,
+                g:limitRGB(state.g-step),
+            };
+        case "Increment_B":
+                return{
+                    ...state,
+                    b:limitRGB(state.b+step)
+                };
+        case "Decrement_B": 
+                return{
+                    ...state,
+                    b:limitRGB(state.b-step),
+                };
+        default:
+                return state;
+    }
+}
+
+export default reducer;
