c3d6393acebb14ae13796e04f80b9c9ccea91357
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2022-03-27 12:30:27 +0500

    Add context

---
+++ vitrina_react/src/Main.jsx
@@ -0,0 +1,12 @@
+
+const Main = () =>{
+     return(
+         <>
+         <p>Hello context</p>
+         <button className="success">
+            Show alert
+         </button>
+         </>
+     )
+}
+export default Main;
