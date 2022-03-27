1af89f09a179aa0513ac4fc8a17369c3ba5b205e
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2022-03-27 14:19:25 +0500

    add clear  contexxt

--- vitrina_react/src/App.jsx
+++ vitrina_react/src/App.jsx
@@ -1,23 +1,10 @@
  
-import { createContext, useState } from 'react';
-import Alert from './Alert';
 import './App.css';
-import Main from './Main';
-
-export const AlertContext =createContext();
 
 function App() {
-  const [alert,setAlert] = useState(false);
-  const toggleAlert = () => setAlert((prev) => !prev);
 
   return (
-    <AlertContext.Provider value={alert}>
-          <div className="app">
-               <Alert  />
-               <Main toggle={toggleAlert}  />
-          </div>
-    </AlertContext.Provider>
+ <></>
   );
 }
-
 export default App;
